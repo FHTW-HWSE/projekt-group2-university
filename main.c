d15dc64d61a2771f11d47397d7d7362b5561adaa
Marwan Abdalla <marwan3m2000@gmail.com>
2023-04-28 11:06:59 +0200

    changed main

--- main.c
+++ main.c
@@ -1,21 +1,6 @@
 #include "headerFiles/headers.h"
 int main()
 {
-    //student *a = createStudent("Marwan", "Georg");
-    student *head = malloc(sizeof(student *));
-    //head = NULL;
-    student *b = createStudent("Basmala", "Elsayad");
-    student *c = createStudent("Marwan", "Abdalla");
-    student *d = createStudent("Marwan", "Marcel");
-    student *e = createStudent("Marwan", "Thomes");
-    student *f = createStudent("Marwan", "Manuel");
-    insertStudentIntoList(head, b);
-    insertStudentIntoList(head, c);
-    insertStudentIntoList(head, d);
-    insertStudentIntoList(head, e);
-    insertStudentIntoList(head, f);
-    printStudentList(head);
-
-
+    printf("Projekt: Sitzplatzgenerator\n");
     return 0;
 }
