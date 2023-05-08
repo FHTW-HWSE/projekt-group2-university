4bf18c833458c874b8b8d3e5580e83e740f5f475
Nurgüzel Keles <keles.nu01@gmail.com>
2023-05-08 20:51:44 +0200

    ICH BIN ZU DUMM UM NACHRICHTEN ZU SCHREIBEN

--- headerFiles/helpers.h
+++ headerFiles/helpers.h
@@ -5,8 +5,6 @@ bool equals(char *a, char *b);
 
 long int generateRandomId();
 
-void timeToString(struct tm *timeptr, char *str, size_t max_len);
-
-char* stringToTime(time_t *time);
+char* timeToString(time_t *time);
 
 #endif
