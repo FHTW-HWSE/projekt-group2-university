4c67bc141857cf8650c7e6df4c84ed727cbbf348
Marwan Abdalla <marwan3m2000@gmail.com>
2023-06-10 11:28:58 +0200

    deleted build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marwan/Technikum/Softwareentwicklung/Sitzplatzgenerator
-# Build directory: /home/marwan/Technikum/Softwareentwicklung/Sitzplatzgenerator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
